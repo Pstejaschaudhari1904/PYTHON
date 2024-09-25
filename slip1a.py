6c927c8dea7dc6b63cc9676b6e6224d5166feff5
Tejas Chaudhari <tejaschaudhari1904@gmail.com>
2024-09-25 18:36:48 +0530

    Add files via upload

---
+++ slip1a.py
@@ -0,0 +1,15 @@
+x=(1,2,3,4)
+y=(3,5,2,1)
+z=(2,2,3,1)
+print("original list")
+print(x)
+print(y)
+print(z)
+print("\nElement wise sum of the said tuples:")
+result=(map(sum,zip(x,y,z)))
+print(result)
+from numpy importarraylst1=[1,5,7]
+lst2=[3,2,1]
+a=array(lst1)
+b=array(lst2)
+print(a+b)
