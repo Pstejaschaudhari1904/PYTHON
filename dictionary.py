e97680af4beb34b6fdd22db6023ee4c5daa3a9cb
Tejas Chaudhari <tejaschaudhari1904@gmail.com>
2024-09-27 08:02:44 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+student={"name":"Dhanu",
+         "addres":"hingangon",
+         "city":"pune",
+         "phonenumber":"7038999999"
+
+         }
+
+ 
+print("your option",student.keys())
+a = input("Enter your information:-\n")
+print(student[a])
+#print(student.get())
